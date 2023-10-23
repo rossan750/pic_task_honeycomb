fced61a561d3aa3851ff90ff6e6f442b2e01f5be
rossan750 <rossan@mail.uc.edu>
2023-10-23 14:42:09 -0400

    Move fixation and pic_task trial into taskBlocks

--- src/timelines/taskBlocks.js
+++ src/timelines/taskBlocks.js
@@ -1,62 +1,29 @@
-// import { pic_trial, preload_trial } from './pic_task';
+// import { pic_trial } from './pic_task';
 import { sequence_variables } from './sequences';
-import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
+// import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
 import jsPsychImageButtonResponse from '@jspsych/plugin-image-button-response';
 import jsPsychPreload from '@jspsych/plugin-preload';
+import { config } from '../config/main';
+import { fixation } from '../trials/fixation';
 
-// const sequence_indexes = Array.from(Array(sequences.length).keys());
-
-function createPicTaskBlocks(jsPsych) {
+function createPicTaskBlocks(jsPsych, is_practice = false) {
   // TO-DO: Get numBlocks out of trial data.
   // const numBlocks = jsPsych.data.get();
 
-  // const sequences = jsPsych.randomization.sampleWithoutReplacement(sequence_indexes, numBlocks);
-  //   jsPsych.data.addProperties({ sequence: random_index + 1 });
-
-  //   const sequence = sequences[random_index];
-  //   const preload_trials = preload_trial(sequence);
-  //   const pic_trials = pic_trial(sequence);
   return {
-    timeline: createPicTaskBlock(jsPsych),
+    timeline: createPicTaskBlock(jsPsych, is_practice),
     timeline_variables: sequence_variables,
     randomize_order: true,
     sample: {
       type: 'without-replacement',
       size: 3,
     },
-    // sample: {
-    //   type: 'custom',
-    //   fn: function (t) {
-    //     console.log(t);
-    //     console.log(jsPsych.data);
-    //     console.log(jsPsych.data.dataProperties);
-    //     var current_node_id = jsPsych.getCurrentTimelineNodeID();
-    //     var data_from_current_node = jsPsych.data.getDataByTimelineNode(current_node_id);
-    //     console.log(data_from_current_node);
-    //     return t;
-    //   },
-    // },
   };
 }
-function createPicTaskBlock(jsPsych) {
-  const pic_task = {
-    type: jsPsychImageButtonResponse,
-    prompt: '<p>Rate the image on a scale from -3 (Dislike Very Much) to 3 (Like Very Much).</p>',
-    choices: ['-3', '-2', '-1', '0', '1', '2', '3'],
-    stimulus: function () {
-      console.log(jsPsych.timelineVariable('stimulus'));
-    },
-  };
-  const fixation = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<p style="font-size: 48px;">+</p>',
-    choices: 'NO_KEYS',
-    trial_duration: 1000,
-    on_trial_start: function (trial) {
-      console.log('on_trial_start', trial);
-    },
-  };
-  console.log(pic_task, fixation);
+function createPicTaskBlock(jsPsych, is_practice) {
+  const pic_task = {};
+  const fixation_trial = fixation(config);
+  console.log(fixation_trial, pic_task);
   return [
     {
       type: jsPsychPreload,
@@ -68,11 +35,34 @@ function createPicTaskBlock(jsPsych) {
         return sequence.map(({ stimulus }) => stimulus);
       },
       data: { sequence_number: jsPsych.timelineVariable('sequence') },
+      // trials: [
+    },
+    {
+      timeline: [
+        // Display fixation dot.
+        fixation_trial,
+        // Display pictures and buttons.
+        {
+          type: jsPsychImageButtonResponse,
+          prompt:
+            '<p>Rate the image on a scale from -3 (Dislike Very Much) to 3 (Like Very Much).</p>',
+          choices: ['-3', '-2', '-1', '0', '1', '2', '3'],
+          stimulus: function () {
+            // const stimulus = jsPsych.timelineVariable('stimulus');
+            // console.log('stimulus', stimulus, jsPsych.getAllTimelineVariables());
+            // return stimulus;
+            console.log(jsPsych.getAllTimelineVariables());
+            // TO-DO: Get stimulus out of timeline_variables.
+          },
+          data: { is_practice },
+        },
+      ],
+      timeline_variables: jsPsych.timelineVariable('images'),
     },
   ];
-  // return [fixation, pic_task];
+  // }
+  // ];
 }
 export { createPicTaskBlocks };
 
-// TO-DO: Need to properly preload all images in a sequence.
 // TO-DO: Add trials field to preload task, which runs pic_task.
