9212e68c8eb35736d5ef986f384156c5061e98e9
rossan750 <rossan@mail.uc.edu>
2023-11-16 13:46:21 -0500

    Sequences are running

--- src/timelines/taskBlocks.js
+++ src/timelines/taskBlocks.js
@@ -6,8 +6,8 @@ import { config } from '../config/main';
 import { fixation } from '../trials/fixation';
 // import { preload_trial } from './pic_task';
 import jsPsychPreload from '@jspsych/plugin-preload';
+let imageIndex = 0;
 
-//TO-DO: Need to pass images as an array of stimulus objects.
 function createPicTaskBlocks(jsPsych, is_practice = false) {
   // TO-DO: Get numBlocks out of trial data.
   // const numBlocks = jsPsych.data.get();
@@ -47,20 +47,27 @@ function createPicTaskBlock(jsPsych, is_practice) {
             '<p>Rate the image on a scale from -3 (Dislike Very Much) to 3 (Like Very Much).</p>',
           choices: ['-3', '-2', '-1', '0', '1', '2', '3'],
           stimulus: function () {
-            console.log(jsPsych.getAllTimelineVariables());
+            console.log(imageIndex);
+            const images = jsPsych.timelineVariable('images')[0];
+            const image = images[imageIndex];
+            return image.stimulus;
             // const stimulus = jsPsych.timelineVariable('stimulus');
-            // console.log('stimulus', stimulus, jsPsych.getAllTimelineVariables());
             // return stimulus;
           },
           data: { is_practice },
         },
       ],
-      // timeline_variables: function () {
-      //   const sequence = jsPsych.timelineVariable('images')[0];
-      //   console.log(sequence);
-      //   return sequence;
-      // },
       timeline_variables: jsPsych.timelineVariable('images')[0],
+      loop_function() {
+        imageIndex++;
+        console.log('loop_function', imageIndex, jsPsych.timelineVariable('images')[0].length);
+        // if we've reached the last distractor item, then stop the loop, otherwise continue
+        if (imageIndex === jsPsych.timelineVariable('images')[0].length) {
+          return false;
+        } else {
+          return true;
+        }
+      },
     },
   ];
 }
